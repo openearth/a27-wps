61d0e55462124f08db07e9bb4f5c4ca634a81dd0
IoannaMi <59479531+IoannaMi@users.noreply.github.com>
2025-12-09 16:14:45 +0100

    add peilfiternaams at getLocations

--- sql/create_table_locatie_agg.sql
+++ sql/create_table_locatie_agg.sql
@@ -3,7 +3,7 @@
 
 drop table if exists gws.locatie_agg;
 create table gws.locatie_agg as
-select st_x(st_transform(l.geom,4326)) as longitude_wgs84, st_y(st_transform(l.geom,4326)) as latitude_wgs84, l.locatie_id, l.locatienaam_master,string_agg(p.peilfilter_id::text, ',') as peilfilterids, l.bron_id, b.dataleverancier
+select st_x(st_transform(l.geom,4326)) as longitude_wgs84, st_y(st_transform(l.geom,4326)) as latitude_wgs84, l.locatie_id, l.locatienaam_master,string_agg(p.peilfilter_id::text, ',') as peilfilterids, string_agg(p.peilfilternaam::text, ',') as peilfilternaams, l.bron_id, b.dataleverancier
 from gws.locatie l 
 join gws.peilfilter p on p.locatie_id = l.locatie_id
 join gws.bron b on b.bron_id = l.bron_id
