3a838d33ecc9987d7d36aa360b3c45e8093284b1
IoannaMi <59479531+IoannaMi@users.noreply.github.com>
2025-12-09 13:04:05 +0100

    Updated get_peilfilter_data_json function to enhance JSON output by including additional fields: dlabel_filter and pompid_filter.

--- sql/get_peilfilter_data_json.sql
+++ sql/get_peilfilter_data_json.sql
@@ -5,7 +5,6 @@ See create_table_locatie_agg.sql as sql that creates the data.
 -- FUNCTION: gws.get_peilfilter_data_json(integer, timestamp without time zone, timestamp without time zone)
 
 -- DROP FUNCTION IF EXISTS gws.get_peilfilter_data_json(integer, timestamp without time zone, timestamp without time zone);
-
 CREATE OR REPLACE FUNCTION gws.get_peilfilter_data_json(
 	peilfilterid integer,
 	start_time timestamp without time zone DEFAULT NULL::timestamp without time zone,
@@ -19,14 +18,16 @@ CREATE OR REPLACE FUNCTION gws.get_peilfilter_data_json(
 AS $BODY$
 SELECT json_build_object(
     'locationproperties',json_build_object(
-						'locatie id', pf.locatie_id,
-	   					'locatie naam',l.locatienaam_master,
-	   					'maaiveld hoogte',l.maaiveldhoogte_cm_nap,
-       					'peilfilter naam', pf.peilfilternaam,
-	   					'peilfilter id', pfd.peilfilter_id, 
-	   					'bovenkantpeil buis',pfd.bovenkant_peilbuis_meetpunt_cm_nap, 
-	   					'bovenkant filter',pfd.bovenkant_filter_cm_nap, 
-	   					'onderkant filter',pfd.onderkant_filter_cm_nap
+						'locatie_id', pf.locatie_id,
+	   					'locatie_naam',l.locatienaam_master,
+	   					'maaiveld_hoogte',l.maaiveldhoogte_cm_nap,
+       					'peilfilter_naam', pf.peilfilternaam,
+	   					'peilfilter_id', pfd.peilfilter_id, 
+	   					'bovenkantpeil_buis',pfd.bovenkant_peilbuis_meetpunt_cm_nap, 
+	   					'bovenkant_filter',pfd.bovenkant_filter_cm_nap, 
+	   					'onderkant_filter',pfd.onderkant_filter_cm_nap,
+	   					'dlabel_filter',  dl."Deltares_peilfilter_interval_label"::text,
+						'pompid_filter', pp.nitg_nummer::text
 	                                      ),
 	'timeseries',json_agg(json_build_object(
  						'datetime',m.datumtijd,
@@ -38,6 +39,8 @@ FROM gws.peilfilter_data pfd
 join gws.peilfilter pf on pf.peilfilter_id = pfd.peilfilter_id
 join gws.locatie l on l.locatie_id = pf.locatie_id
 join gws.meting m on m.peilfilter_id = pfd.peilfilter_id
+LEFT JOIN public."Dlabels_draft" dl ON dl.peilfilter_id = pfd.peilfilter_id
+LEFT JOIN public.pompproef_draft pp ON pp.peilfilter_id = pfd.peilfilter_id
 where pfd.peilfilter_id = peilfilterid 
 and m.datumtijd >= coalesce(start_time,m.datumtijd - interval '100 year') 
 and (end_time is NULL or m.datumtijd <= end_time)
@@ -48,8 +51,13 @@ group by pf.locatie_id,
 		 pfd.peilfilter_id,
 		 pfd.bovenkant_peilbuis_meetpunt_cm_nap,
 		 pfd.bovenkant_filter_cm_nap,
-		 pfd.onderkant_filter_cm_nap
+		 pfd.onderkant_filter_cm_nap,
+		 dl."Deltares_peilfilter_interval_label"::text,
+		 pp.nitg_nummer::text
 $BODY$;
 
 ALTER FUNCTION gws.get_peilfilter_data_json(integer, timestamp without time zone, timestamp without time zone)
     OWNER TO hendrik_gt;
+    
+   
+ 
